39188c0a4f8c2f249e4a14afd88ee4d8e853dc39
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-20 19:26:38 -0300

    add-barchart

--- src/dashboard.js
+++ src/dashboard.js
@@ -47,6 +47,19 @@ function Dashboard() {
           pieHole: 0.4
         }}
       />
+      <Chart
+        width={'400px'}
+        height={'300px'}
+        chartType={'BarChart'}
+        data={dados}
+        options={{
+          title: TITULO,
+          chartArea: { width: '50%' },
+          hAxis: { title: 'Quantidade' },
+          vAxis: { title: 'Mes' },
+          animation: { duration: 1000, easing: 'out', startup: true }
+        }}
+      />
     </div>
 
   );
